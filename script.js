4ec5ebeb34aa4b57fb67d9eeac550a6af7f91158
Pierluigi92 <pierluigi.mancino@hotmail.com>
2026-05-10 18:06:39 +0200

    Adaptable grid squares based on input prompt

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const containerGrid = document.querySelector('#container-grid');
-const quadratiPerRiga = 16;
-const totalQuadrati = quadratiPerRiga * quadratiPerRiga;
 
-for (let i = 0; i < totalQuadrati; i++) {
-    const quadrati = document.createElement('div');
-    quadrati.classList.add('quadrati');
-    containerGrid.appendChild(quadrati);
+function creaGriglia(lato) {
+    containerGrid.innerHTML = "";
+    const dimensione = 100 / lato;
+    const totalQuadrati = lato * lato;
+    for (let i = 0; i < totalQuadrati; i++) {
+        const quadrato = document.createElement('div');
+        quadrato.classList.add('quadrati');
+        quadrato.style.flex = `1 1 ${dimensione}%`
+        quadrato.style.height = `${dimensione}%`;    
+        quadrato.addEventListener('mouseenter', () => {
+            quadrato.style.backgroundColor = 'black';
+        });
+        containerGrid.appendChild(quadrato);
+    }
 }
+
+document.querySelector('#button-grid').addEventListener('click', () => {
+    let risposta = prompt("Inserisci numero quadrati per lato, (max 100)", 16); 
+    let nuovoLato = parseInt(risposta);
+    if ( nuovoLato > 0 && nuovoLato <= 100) {
+        creaGriglia(nuovoLato);
+    } else {
+        alert("Inserisci un numero valido tra 1 e 100");
+    }
+});
+
+creaGriglia(16);
+
+
+
+
+
